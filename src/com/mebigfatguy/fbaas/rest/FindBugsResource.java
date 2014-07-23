e928ad84ea51b99280e02e42fadbdcd690f1f634
Dave Brosius <dbrosius@mebigfatguy.com>
2014-07-22 22:39:47 -0400

    oi

--- src/com/mebigfatguy/fbaas/rest/FindBugsResource.java
+++ src/com/mebigfatguy/fbaas/rest/FindBugsResource.java
@@ -21,7 +21,6 @@ import java.util.concurrent.ArrayBlockingQueue;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
