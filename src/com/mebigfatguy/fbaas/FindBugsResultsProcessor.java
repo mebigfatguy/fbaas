e7343af4ad2aec24163e7313aa73f4cb8fd94f2c
Dave Brosius <dbrosius@mebigfatguy.com>
2014-12-28 11:05:47 -0500

    add new results processing files

---
+++ src/com/mebigfatguy/fbaas/FindBugsResultsProcessor.java
@@ -0,0 +1,29 @@
+/** fbaas - FindBugs as a Service. 
+ * Copyright 2014 MeBigFatGuy.com 
+ * Copyright 2014 Dave Brosius 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations 
+ * under the License. 
+ */
+package com.mebigfatguy.fbaas;
+
+import java.util.Locale;
+
+public class FindBugsResultsProcessor {
+
+    public Results getResults(Locale locale, FBJob job) {
+        String status = Bundle.getString(locale, Bundle.Processing);
+        Results r = new Results(status, null);
+        
+        return r;
+    }
+}
---
+++ src/com/mebigfatguy/fbaas/Results.java
@@ -0,0 +1,47 @@
+/** fbaas - FindBugs as a Service. 
+ * Copyright 2014 MeBigFatGuy.com 
+ * Copyright 2014 Dave Brosius 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations 
+ * under the License. 
+ */
+package com.mebigfatguy.fbaas;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+public class Results {
+
+    private String status;
+    List<Bug> bugs;
+    
+    public Results(String status, List<Bug> bugs) {
+        this.status = status;
+        this.bugs = bugs;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public List<Bug> getBugs() {
+        return bugs;
+    }
+    
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+}
