b10ec1466d71a5de621e343e689e624f3e254ed4
Dave Brosius <dbrosius@mebigfatguy.com>
2017-04-20 03:31:46 -0400

    switch over to Paths

--- src/com/mebigfatguy/fbaas/Status.java
+++ src/com/mebigfatguy/fbaas/Status.java
@@ -18,11 +18,11 @@ package com.mebigfatguy.fbaas;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -35,60 +35,72 @@ public class Status {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Status.class);
 
-    private static final File FBAAS_DIR = new File(System.getProperty("user.home"), ".fbaas");
-    private static final File PROCESSING_DIR = new File(FBAAS_DIR, "processing");
-    private static final File REPORT_DIR = new File(FBAAS_DIR, "reports");
+    private static final Path FBAAS_DIR = Paths.get(System.getProperty("user.home"), ".fbaas");
+    private static final Path PROCESSING_DIR = FBAAS_DIR.resolve("processing");
+    private static final Path REPORT_DIR = FBAAS_DIR.resolve("reports");
     private static final Duration MAX_PROCESSING_TIME = Duration.of(1, ChronoUnit.HOURS);
 
     static {
-        FBAAS_DIR.mkdirs();
-        PROCESSING_DIR.mkdirs();
-        REPORT_DIR.mkdirs();
+        try {
+            Files.createDirectories(FBAAS_DIR);
+            Files.createDirectories(PROCESSING_DIR);
+            Files.createDirectories(REPORT_DIR);
+        } catch (IOException e) {
+            LOGGER.error("Failed to create basic directories: {} or {} or {}", FBAAS_DIR, PROCESSING_DIR, REPORT_DIR);
+        }
     }
 
     private Status() {
     }
 
     public static boolean isProcessing(Artifact job) {
-        File procFile = new File(PROCESSING_DIR, job.fileName());
-        if (!procFile.exists() || !procFile.isFile()) {
+        try {
+            Path procPath = PROCESSING_DIR.resolve(job.fileName());
+            if (!Files.exists(procPath) || Files.isDirectory(procPath)) {
+                return false;
+            }
+
+            Instant now = Instant.now();
+            Instant fileTime = Instant.ofEpochMilli(Files.getLastModifiedTime(procPath).toMillis());
+            Duration d = Duration.between(fileTime, now);
+            return (d.compareTo(MAX_PROCESSING_TIME) < 0);
+        } catch (IOException e) {
             return false;
         }
-
-        Instant now = Instant.now();
-        Instant fileTime = Instant.ofEpochMilli(procFile.lastModified());
-        Duration d = Duration.between(fileTime, now);
-        return (d.compareTo(MAX_PROCESSING_TIME) < 0);
     }
 
     public static void setProcessing(Artifact job) {
         try {
-            File procFile = new File(PROCESSING_DIR, job.fileName());
-            procFile.delete();
-            procFile.createNewFile();
-            procFile.deleteOnExit();
+            Path procPath = PROCESSING_DIR.resolve(job.fileName());
+            Files.delete(procPath);
+            Files.createFile(procPath);
+            procPath.toFile().deleteOnExit();
         } catch (IOException e) {
             LOGGER.error("Failed to create processing file for {}", job, e);
         }
     }
 
     public static String getProcessingFailed(Locale locale, Artifact job) {
-        File procFile = new File(PROCESSING_DIR, job.fileName());
-        if (procFile.length() == 0) {
-            return null;
-        }
+        try {
+            Path procPath = PROCESSING_DIR.resolve(job.fileName());
+            if (Files.size(procPath) == 0L) {
+                return null;
+            }
 
-        try (BufferedReader r = Files.newBufferedReader(procFile.toPath(), StandardCharsets.UTF_8)) {
-            return Bundle.getString(locale, Bundle.Failure, r.readLine());
+            try (BufferedReader r = Files.newBufferedReader(procPath, StandardCharsets.UTF_8)) {
+                return Bundle.getString(locale, Bundle.Failure, r.readLine());
+            } catch (IOException e) {
+                LOGGER.error("Failed reading failure message in processing file for {}", job, e);
+                return Bundle.getString(locale, Bundle.Failure, job);
+            }
         } catch (IOException e) {
-            LOGGER.error("Failed reading failure message in processing file for {}", job, e);
-            return Bundle.getString(locale, Bundle.Failure, job);
+            return null;
         }
     }
 
     public static void setProcessingFailed(Artifact job, Exception e) {
-        File procFile = new File(PROCESSING_DIR, job.fileName());
-        try (BufferedWriter bw = Files.newBufferedWriter(procFile.toPath(), StandardCharsets.UTF_8)) {
+        Path procPath = PROCESSING_DIR.resolve(job.fileName());
+        try (BufferedWriter bw = Files.newBufferedWriter(procPath, StandardCharsets.UTF_8)) {
             bw.write(e.getMessage());
         } catch (IOException ioe) {
             LOGGER.error("Failed writing failure message to processing file for {}", job, ioe);
@@ -96,21 +108,27 @@ public class Status {
     }
 
     public static Path getReportFile(Artifact job) {
-        return new File(REPORT_DIR, job.fileName()).toPath();
+        return REPORT_DIR.resolve(job.fileName());
     }
 
     public static boolean hasReport(Artifact job) {
-        File reportFile = new File(REPORT_DIR, job.fileName());
-        return (reportFile.exists() && reportFile.isFile());
+        Path reportPath = REPORT_DIR.resolve(job.fileName());
+        return (Files.exists(reportPath) && !Files.isDirectory(reportPath));
     }
 
     public static void deleteProcessingFile(Artifact job) {
-        File procFile = new File(PROCESSING_DIR, job.fileName());
-        procFile.delete();
+        try {
+            Path procPath = PROCESSING_DIR.resolve(job.fileName());
+            Files.delete(procPath);
+        } catch (IOException e) {
+        }
     }
 
     public static void deleteReport(Artifact job) {
-        File reportFile = new File(REPORT_DIR, job.fileName());
-        reportFile.delete();
+        try {
+            Path reportPath = REPORT_DIR.resolve(job.fileName());
+            Files.delete(reportPath);
+        } catch (IOException e) {
+        }
     }
 }
