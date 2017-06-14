fbea66eba28411c245f6fe668829cc755d91aaed
Dave Brosius <dbrosius@mebigfatguy.com>
2017-06-14 18:37:39 -0400

    cleanup throws clauses

--- src/main/java/com/mebigfatguy/fbaas/FindBugsProcessor.java
+++ src/main/java/com/mebigfatguy/fbaas/FindBugsProcessor.java
@@ -80,7 +80,7 @@ public class FindBugsProcessor implements Runnable {
                             FileVisitor<Path> fv = new SimpleFileVisitor<Path>() {
 
                                 @Override
-                                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
                                     Files.delete(file);
                                     return FileVisitResult.CONTINUE;
                                 }
